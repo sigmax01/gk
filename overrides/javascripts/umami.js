7fc4f1b43bc5bdd287ad1f85d2aaf41e9f98c185
wenzexu <ricol.xwz@outlook.com>
2024-09-04 23:38:18 +1000

    update umami.js

--- overrides/javascripts/umami.js
+++ overrides/javascripts/umami.js
@@ -1,13 +1,13 @@
 function umami() {
-  var script = document.createElement("script");
-  script.defer = true;
-  script.src = "https://umami.ricolxwz.io/script.js";
-  script.setAttribute(
-    "data-website-id",
-    "e70268ae-b4d1-49e4-b8ef-8fa95a12a8f2"
-  );
-
-  document.head.appendChild(script);
+  if (window.location.hostname !== "localhost" && window.location.hostname !== "127.0.0.1") {
+    var script = document.createElement("script");
+    script.defer = true;
+    script.src = "https://umami.ricolxwz.io/script.js";
+    script.setAttribute(
+      "data-website-id",
+      "e70268ae-b4d1-49e4-b8ef-8fa95a12a8f2"
+    );
+    document.head.appendChild(script);
+  }
 }
-
 umami();
